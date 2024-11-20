d04b0be8ed8a96204dd7e88a5f873870352e8ddd
Vishnu Mohan <vishnumohanmfc@gmail.com>
2024-11-20 14:51:56 +0530

    auth correction

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,24 +1,19 @@
 import jwt from "jsonwebtoken";
 import User from "../model/userModel.js";
-
-// Middleware to protect routes
 const protect = async (req, res, next) => {
     let token;
 
     try {
-        // Check for token in the Authorization header or cookies
         if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
             token = req.headers.authorization.split(" ")[1]; // Extract token from Authorization header
         } else if (req.cookies && req.cookies.jwt) {
             token = req.cookies.jwt; // Extract token from cookies
         }
 
-        // If no token, deny access
         if (!token) {
             return res.status(401).json({ message: "Unauthorized. No token provided." });
         }
 
-        // Verify the token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         // Attach the user information to the request object
@@ -28,19 +23,18 @@ const protect = async (req, res, next) => {
             return res.status(404).json({ message: "User not found." });
         }
 
-        next(); // Proceed to the next middleware
+        next();
     } catch (error) {
         console.error(error);
         res.status(401).json({ message: "Unauthorized. Invalid token." });
     }
 };
 
-// Middleware to check if the user is an admin
 const isAdmin = (req, res, next) => {
     if (req.user && req.user.isAdmin) {
-        next(); // User is an admin, allow access
+        next();
     } else {
-        res.status(403).json({ message: "Access denied. Admins only." }); // Forbidden
+        res.status(403).json({ message: "Access denied. Admins only." });
     }
 };
 
